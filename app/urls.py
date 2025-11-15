f92473f606906536e1e8f0efd0ff2648d652e38c
andrew <andrew@Andrews-MacBook-Pro.local>
2025-11-15 17:19:44 +1100

    modified:   app/static/app/weather.css 	modified:   app/templates/app/index.html 	modified:   app/templates/app/weather.html 	modified:   app/urls.py 	modified:   app/views.py 	modified:   db.sqlite3 	deleted:    worldcities.fld/filelist.xml 	deleted:    worldcities.fld/sheet001.htm 	deleted:    worldcities.fld/stylesheet.css 	deleted:    worldcities.fld/tabstrip.htm
    	app/management/

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("weather/", views.weather, name="weather"),
+    path("countries/", views.countries, name='countries')
 ]
--- app/views.py
+++ app/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
+from app.models import Data
+from django.http import JsonResponse, HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, "app/index.html")
+    print(Data.objects.values_list('city', 'country'))
+    cityData = Data.objects.values_list('city')
+    countryData = Data.objects.values_list('country')
+
+    d = list( dict.fromkeys(countryData) )
+    uniqueCountryData = sorted(d, reverse=False)
+    f = list ( dict.fromkeys(cityData) )
+    uniqueCityData = sorted(f, reverse=False)
+
+    def getCities(country):
+        return Data.objects.filter(country=country)
+        
+
+    return render(request, "app/index.html", {
+        "city": getCities,
+        "country": [i[0] for i in uniqueCountryData],
+    })
+
 
 def weather(request):
     longtitude = request.GET.get("long")
@@ -12,3 +31,13 @@ def weather(request):
         "longitude": longtitude,
         "latitude": latitude,
     })
+
+def countries(request):
+    country = request.GET.get("country")
+    myData = Data.objects.filter(country=country)
+    v = ",".join(i.city for i in myData)
+
+   
+    result = v
+    print(result)
+    return JsonResponse(result, safe=False)
